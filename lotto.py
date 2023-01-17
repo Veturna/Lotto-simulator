8f17c955188e0e0aa2e04d74f2e22d7c982266a7
Veturna <dpullo@outlook.com>
2023-01-17 19:14:15 +0100

    lotto without documentation

---
+++ lotto.py
@@ -0,0 +1,42 @@
+import random
+
+def get_number():
+    while True:
+        try:
+            user_choice = int(input("Give the number from 1 to 49: "))
+            break
+        except ValueError:
+            print("It's not a number")
+
+    return user_choice
+
+def get_numbers():
+    numbers = []
+    while len(numbers) < 6:
+        number = get_number()
+        if number not in numbers and 0 < number <= 49:
+            numbers.append(number)
+
+    return sorted(numbers)
+
+def random_numbers():
+    computer_numbers = list(range(1, 49))
+    random.shuffle(computer_numbers)
+    return computer_numbers[:6]
+
+def lotto():
+    user_numbers = get_numbers()
+    print(f"Your numbers: {user_numbers}")
+
+    computer_numbers = random_numbers()
+    print(f"Winning numbers: {computer_numbers}")
+
+    hits = 0
+    for number in user_numbers:
+        if number in random_numbers():
+            hits += 1
+
+    print(f"You hit {hits} {'number' if hits == 1 or hits == 0 else 'numbers'}!")
+
+if __name__ == '__main__':
+    lotto()
